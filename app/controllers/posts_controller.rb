041d3ff45ea56ed34436f5b6daec919695fd7ada
inatukitae <fuvy5753@gmail.com>
2026-08-13 14:50:21 +0900

    feat: 投稿機能（ドロワーUI・一覧表示・トップページリンク）の実装

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,48 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user! 
+  before_action :set_post, only: %i[show edit update destroy]
+
+  def index
+    @posts = current_user.posts.order(created_at: :desc)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path, notice: "投稿を作成しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: "投稿を更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice: "投稿を削除しました", status: :see_other
+  end
+
+  private
+
+  def set_post
+    @post = current_user.posts.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:event, :emotion, :issue)
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -33,8 +33,8 @@ module MyPortfolio
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
 
     # Don't generate system test files.
     config.generators.system_tests = nil
